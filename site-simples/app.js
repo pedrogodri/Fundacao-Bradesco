d885d369a5568d7ac7bfba063dc0b18c38b0ddee
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-01-11 13:08:03 -0300

    Finalizando o Curso de Site SImples

--- site-simples/app.js
+++ site-simples/app.js
@@ -1,1 +1,16 @@
-'use strict';
+'use strict'
+
+const btnTema = document.querySelector('.btn');
+
+btnTema.addEventListener('click', function(){
+    document.body.classList.toggle('tema-escuro');
+
+    var classeNome = document.body.className;
+    if(classeNome == "tema-claro") {
+        this.conteudoTexto = "Escuro";
+    }
+    else {
+        this.conteudoTexto = "Claro";
+    }
+    console.log('Classe Nome Atual: ' + classeNome);
+});
